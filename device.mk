ae70c441f3b210c4d7610bc80031a6deae89c67e
Fars <101596708+farsxdev@users.noreply.github.com>
2024-07-07 18:13:03 +0300

    camellial: Speed preopt on SystemUI

--- device.mk
+++ device.mk
@@ -99,6 +99,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
